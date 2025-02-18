3df13a1ab416d3b818d03652e8161538ecc681f1
Ilia <ilia.loviagin@gmail.com>
2025-02-18 22:54:17 +0300

    Contacts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
+import Contacts from "../components/Contacts/Contacts";
 import Hero from "../components/Hero-2/Hero";
 import HowItWorks from "../components/HowItWorks-2/HowItWorks";
 import LikeThis from "../components/LikeThis/LikeThis";
 import Portfolio from "../components/Portfoilio/Portfolio";
-import WhyChooseUs from "../components/WhyChooseUs-2/WhyChooseUs";
 // import styles from "./page.module.css";
 
 export default function Home() {
@@ -10,9 +10,9 @@ export default function Home() {
         <main>
             <Hero />
             <HowItWorks />
-            {/* <WhyChooseUs /> */}
             <LikeThis />
             <Portfolio />
+            <Contacts />
         </main>
     );
 }
