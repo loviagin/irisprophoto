819357815cea8bc8cf9f79846a8dd8aa9a464fc3
Ilia <ilia.loviagin@gmail.com>
2025-02-18 14:11:56 +0300

    Orders updates

--- app/api/telegram-webhook/route.ts
+++ app/api/telegram-webhook/route.ts
@@ -5,11 +5,11 @@ import admin from "firebase-admin";
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
             // Если privateKey содержит переносы строк, заменяем их корректно
-            privateKey: process.env.FIREBASE_PRIVATE_KEY
-                ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+            privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY
+                ? process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
                 : undefined,
         }),
     });
