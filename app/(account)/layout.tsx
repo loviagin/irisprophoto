b94b24f5e8124dee82dd39449d55f78293188bf0
Ilia <ilia.loviagin@gmail.com>
2025-03-21 14:36:10 +0300

    Added GTM

--- app/(account)/layout.tsx
+++ app/(account)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "../globals.css";
 import Header from "./components/Header/Header";
+import Script from "next/script";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -23,6 +24,21 @@ export default function RootLayout({
         <Header />
         {children}
       </body>
+
+      {/* Google Tag Manager */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NGGHDDMX');
+          `,
+        }}
+      />
     </html>
   );
 }
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 import "../globals.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Script from "next/script";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -25,6 +26,21 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+
+      {/* Google Tag Manager */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NGGHDDMX');
+          `,
+        }}
+      />
     </html>
   );
 }
