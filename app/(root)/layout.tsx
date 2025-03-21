151970c492dc67a42385ccd3049795ac8406dd1c
Ilia <ilia.loviagin@gmail.com>
2025-03-21 19:18:54 +0300

    Added yandex metrika

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -26,18 +26,16 @@ export default function RootLayout({
         {/* Yandex.Metrika counter */}
         <Script id="yandex-metrika" strategy="afterInteractive">
           {`
-          function(m,e,t,r,i,k,a){m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+            k.async=1;k.src=r;a.parentNode.insertBefore(k,a)})
             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
             ym(99836759, "init", {
-              clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
-          });
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true
+            });
           `}
         </Script>
       </head>
