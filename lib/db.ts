899c918c80e99d35899397181520626f91dcefcd
Ilia <ilia.loviagin@gmail.com>
2025-03-27 23:23:34 +0300

    Added new api

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,7 @@ declare global {
   var mongoose: { conn: any; promise: any } | undefined
 }
 
-const MONGODB_URI = process.env.NEXT_PUBLIC_MONGODB_URI!
+const MONGODB_URI = process.env.MONGODB_URI!
 
 let cached = global.mongoose || { conn: null, promise: null }
 
