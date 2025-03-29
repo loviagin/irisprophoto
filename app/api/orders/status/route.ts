bc3698026f14688ffc90bf1c9a4f11a6e2695a70
Ilia <ilia.loviagin@gmail.com>
2025-03-29 18:08:49 +0300

    Added orders/status API

--- app/api/orders/status/route.ts
+++ app/api/orders/status/route.ts
@@ -30,7 +30,7 @@ export async function POST(req: NextRequest) {
             page_id: orderId,
             properties: {
                 [field]: {
-                    select: {
+                    status: {
                         name: data || 'None',
                     },
                 },
