fac057e1c7f4dc56158b0f461e14d66d203fd301
Ilia <ilia.loviagin@gmail.com>
2025-04-01 23:52:46 +0300

    Updated Booking modal

--- app/(root)/components/BookingModal/BookingModal.tsx
+++ app/(root)/components/BookingModal/BookingModal.tsx
@@ -126,48 +126,44 @@ export default function BookingModal({
 
     console.log(JSON.stringify(order))
 
-    // const response = await fetch("/api/orders", {
-    //   method: "POST",
-    //   headers: { "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`, "Content-Type": "application/json" },
-    //   body: JSON.stringify(order),
-    // });
-
-    // const result = await response.json();
-
-    // if (result.success) {
-    // alert("Thanks for your order! We will contact you soon.");
-
-    if (formDataToSend.email) {
-      const res = await fetch('/api/email-order', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`
-        },
-        body: JSON.stringify({ name: formDataToSend.name, email: formDataToSend.email }),
-      });
-
-      const result = await res.json();
-
-      if (result.success) {
-        alert("Thanks for your order! We will contact you soon.");
-      } else {
-        alert("❌ Error: " + result.error);
-      }
-    }
-
-    onClose();
-    setFormData({
-      name: "",
-      email: "",
-      phone: "",
-      shootingType: "wedding",
-      dateTime: new Date(),
-      details: ""
+    const response = await fetch("/api/orders", {
+      method: "POST",
+      headers: { "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`, "Content-Type": "application/json" },
+      body: JSON.stringify(order),
     });
-    // } else {
-    //   alert("❌ Error: " + result.error);
-    // }
+
+    const result = await response.json();
+
+    if (result.success) {
+      alert("Thanks for your order! We will contact you soon.");
+
+      if (formDataToSend.email) {
+        const res = await fetch('/api/email-order', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`
+          },
+          body: JSON.stringify({ name: formDataToSend.name, email: formDataToSend.email }),
+        });
+
+        const result = await res.json();
+      }
+
+      alert("Thanks for your order! We will contact you soon.");
+     
+      onClose();
+      setFormData({
+        name: "",
+        email: "",
+        phone: "",
+        shootingType: "wedding",
+        dateTime: new Date(),
+        details: ""
+      });
+    } else {
+      alert("❌ Error: " + result.error);
+    }
 
     onClose()
   }
