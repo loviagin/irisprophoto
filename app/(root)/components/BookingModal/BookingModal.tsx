d6c76bf6adcdb9cd940bdd19666c0b29817ccd88
Ilia <ilia.loviagin@gmail.com>
2025-04-02 18:02:01 +0300

    Added Time in Booking Modal

--- app/(root)/components/BookingModal/BookingModal.tsx
+++ app/(root)/components/BookingModal/BookingModal.tsx
@@ -171,15 +171,15 @@ export default function BookingModal({
       createdAt: new Date().toISOString()
     }
 
-    const response = await fetch("/api/orders", {
-      method: "POST",
-      headers: { "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`, "Content-Type": "application/json" },
-      body: JSON.stringify(order),
-    });
+    // const response = await fetch("/api/orders", {
+    //   method: "POST",
+    //   headers: { "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`, "Content-Type": "application/json" },
+    //   body: JSON.stringify(order),
+    // });
 
-    const result = await response.json();
+    // const result = await response.json();
 
-    if (result.success) {
+    // if (result.success) {
 
       if (formDataToSend.email) {
         const res = await fetch('/api/email-order', {
@@ -198,29 +198,29 @@ export default function BookingModal({
         const result = await res.json();
       }
 
-      try {
-        const response = await fetch("/api/bookings", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name: formData.name,
-            contact,
-            notionId,
-            bookingDateTime: selectedDateTime.toISOString()
-          }),
-        });
+    //   try {
+    //     const response = await fetch("/api/bookings", {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify({
+    //         name: formData.name,
+    //         contact,
+    //         notionId,
+    //         bookingDateTime: selectedDateTime.toISOString()
+    //       }),
+    //     });
 
-        const result = await response.json();
-      } catch (error) {
-        console.error('Error creating booking:', error);
-        alert("An error occurred while creating an order. Please try again later.");
-      }
+    //     const result = await response.json();
+    //   } catch (error) {
+    //     console.error('Error creating booking:', error);
+    //     alert("An error occurred while creating an order. Please try again later.");
+    //   }
 
-      console.log(JSON.stringify(order))
+    //   console.log(JSON.stringify(order))
 
-      alert("Thanks for your order! We will contact you soon.");
+    //   alert("Thanks for your order! We will contact you soon.");
 
       onClose();
       setFormData({
@@ -231,9 +231,9 @@ export default function BookingModal({
         dateTime: new Date(),
         details: ""
       });
-    } else {
-      alert("❌ Error: " + result.error);
-    }
+    // } else {
+    //   alert("❌ Error: " + result.error);
+    // }
 
     onClose()
   };
